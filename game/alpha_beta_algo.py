e7dfa29e4f42cc08e074577b3c1b7a3ff2400a71
Timurs Vahitovs <timurs.vahitovs@edu.rtu.lv>
2025-03-24 21:55:01 +0200

    + fails ar kopīgām funkcijām algoritmos

--- game/alpha_beta_algo.py
+++ game/alpha_beta_algo.py
@@ -2,13 +2,13 @@ from collections import Counter
 from data_structurs import GameTreeNode
 from ai_func import AiFunc
 
-class MinimaxAI(AiFunc):
+class AlphaBetaAI(AiFunc):
     def __init__(self, game_logic, max_depth=3):
         self.game_logic = game_logic
         self.max_depth = max_depth
 
     def getBestMove(self):
-        #print("Minimax")
+        #print("Alpha-Beta")
         print(f"Current Sequence: {self.game_logic.sequence}")  # for debug
         print(f"Current Scores: {self.game_logic.scores}")  # for debug
         print(f"Player Turn: {self.game_logic.player_turn}")  # for debug
@@ -23,33 +23,40 @@ class MinimaxAI(AiFunc):
 
         bestValue = -float('inf')
         bestMove = None
+        alpha = -float('inf')
+        beta = float('inf')
 
         for child in root.children:
-            value = self._minimax(child, depth = 1, isMaximizing = False)
+            value = self._alphabeta(child, depth = 1, alpha=alpha, beta=beta, isMaximizing = False)
             if value > bestValue:
                 bestValue = value
                 bestMove = self._getMoveFromChild(root, child)
+            alpha = max(alpha, bestValue)
 
         print(f"Best Move: {bestMove}")
         return bestMove
 
-    def _minimax(self, node, depth, isMaximizing):
+    def _alphabeta(self, node, depth, alpha, beta, isMaximizing):
         if depth == self.max_depth or not node.sequence:
             return self._evaluate(node)
 
         if isMaximizing:
             value = -float('inf')
-            
+
             for child in node.children:
-                value = max(value, self._minimax(child, depth + 1, False))
-                
+                value = max(value, self._alphabeta(child, depth + 1, alpha, beta, False))
+                alpha = max(alpha, value)
+                if alpha >= beta: #beta nogriezšana
+                    break
+
             return value
         else:
             value = float('inf')
-            
-            for child in node.children:
-                value = min(value, self._minimax(child, depth + 1, True))
-                
-            return value  
 
-    
+            for child in node.children:
+                value = min(value, self._alphabeta(child, depth + 1, alpha, beta, True))
+                beta = min(beta, value)
+                if beta <= alpha: #alpha nogriezšana
+                    break
+
+            return value
